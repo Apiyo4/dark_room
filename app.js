a4f753df5b59845c8a98d50fc4ce250f00a055a5
john mutavi <jonnygovish@gmail.com>
2020-06-10 11:35:03 +0300

    Create a route to handle file upload

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Import express package
 const express = require('express');
+const upload = require('./upload');
 
 
 // Initiliaze express
@@ -17,6 +18,17 @@ app.get('/', (req,res)=>{
     res.render('index');
 })
 
+// route to handle image upload
+app.post('/upload', (req,res)=>{
+    upload(req, res, (err)=>{
+        if (err){
+            console.log(err)
+        }else{
+            console.log(req.file);
+            res.send('test');
+        }
+    })
+})
 
 // Define the port number
 const PORT = 5000;
