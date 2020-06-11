61bd492a48a135e880135cc8796b42c44b518e7e
john mutavi <jonnygovish@gmail.com>
2020-06-11 11:52:27 +0300

    Register the index router in app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Import express package
 const express = require('express');
-const upload = require('./upload');
 
+let indexRouter = require('./routes/index');
 
 // Initiliaze express
 const app = express();
@@ -13,22 +13,9 @@ app.set('view engine', 'ejs');
 // Set a static folder
 app.use(express.static('public'));
 
+// Define the index router
+app.use('/', indexRouter);
 
-app.get('/', (req,res)=>{
-    res.render('index');
-})
-
-// route to handle image upload
-app.post('/upload', (req,res)=>{
-    upload(req, res, (err)=>{
-        if (err){
-            console.log(err)
-        }else{
-            console.log(req.file);
-            res.send('test');
-        }
-    })
-})
 
 // Define the port number
 const PORT = 5000;
