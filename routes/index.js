f7b77498cb3b6d3d7e4a0da7a4e74dcbc30d2b36
john mutavi <jonnygovish@gmail.com>
2020-06-22 11:58:45 +0300

    Logic for saving and retrieving an image

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const upload = require('../upload');
+const Photo = require('../models/photo');
 
+// Home page router
 router.get('/', (req,res)=>{
-    res.render('index');
+    Photo.find({}, (error, images)=>{
+        if (error){
+            console.log(error);
+        }else{
+            res.render('index', { images:images, msg: req.query.msg});
+        }
+    })
 })
 
 // route to handle image upload
@@ -11,13 +19,28 @@ router.post('/upload', (req,res)=>{
     upload(req, res, (err)=>{
         if (err){
             console.log(err)
-            res.render('index', {msg: err})
+            // Redirect to the index page and pass a msg to the template
+            res.redirect(`/?msg=${err}`);
         }else if(req.file === undefined){
-            res.render('index', {msg: 'Error: No file selected!!'})
+            // Redirect to the index page and pass a msg to the template
+            res.redirect('/?msg=Error: No file selcted!');
         }else{
-            res.render('index', {file: 'images/' + req.file.filename})
+            // Create a new photo
+            let newPhoto = new Photo({
+                name: req.file.filename,
+                path: 'images/' + req.file.filename,
+                size: req.file.size
+            })
+
+            // Save newPhoto in the database
+            newPhoto.save()
+
+            // Redirect to the index page and pass a msg to the template
+            res.redirect('/?msg=File uploaded successfully');
         }
     })
 })
 
 module.exports = router;
+
+
