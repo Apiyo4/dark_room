96e3a0d50c79649efba6e69b62419869967f788e
john mutavi <jonnygovish@gmail.com>
2020-06-24 09:24:52 +0300

    Single image route handlers

---
+++ routes/image.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const Photo = require('../models/photo');
+
+router.get('/:id', (req,res)=>{
+
+    Photo.findById(req.params.id, (error, image)=>{
+        if (error){
+            console.log(error)
+        }else{
+            // console.log(image)
+            res.render('single_image', {image:image})
+        }
+    })
+})
+
+// route to handle put request
+router.put('/:id', (req,res)=>{
+    
+    Photo.updateOne({_id: req.params.id}, {
+        $set: {
+            name: req.body.name
+        }
+    },{upsert:true}, (err, image)=>{
+        if (err){
+            console.log(err)
+        }else{
+            res.redirect('/')
+        }
+    })
+
+})
+
+module.exports = router;
